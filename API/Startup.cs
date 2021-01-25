279a17a784061b1747c3f35bf272abdb3a791dc3
Iryna Lucky <ikabanen@yahoo.com>
2021-01-24 16:07:20 -0800

    Added filters, sorting and paging

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,47 +24,26 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            // services.AddScoped<IProductRepository, ProductRepository>();
-            // services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            // });
-
-            // services.Configure<ApiBehaviorOptions>(options => 
-            // {
-            //     options.InvalidModelStateResponseFactory = actionContext => 
-            //     {
-            //         var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0)
-            //         .SelectMany(x => x.Value.Errors).Select(x => x.ErrorMessage).ToArray();
-
-            //         var errorResponse = new ApiValidationErrorResponse
-            //         {
-            //             Errors = errors
-            //         };
-            //         return new BadRequestObjectResult(errorResponse);
-            //     };
-            // });
+            services.AddDbContext<StoreContext>(x => 
+            x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            
             services.AddApplicationServices();
-            // services.AddSwaggerGen(c => 
-            // {
-            //   c.SwaggerDoc("v1", new OpenApiInfo {Title = "YumSoap API", Version = "v1"});  
-            // });
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            // }
             app.UseMiddleware<ExceptionMiddleware>();
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
@@ -73,12 +52,10 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
-
-            // app.UseSwagger();
-            // // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            // app.UseSwaggerUI(c => {c.SwaggerEndpoint("/swagger/v1/swagger.json", "YumSoap API v1");});
 
             app.UseEndpoints(endpoints =>
             {
